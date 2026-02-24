d9040cf6f4eb094cb3de182392dfbac85bf8abb5
Vaibhav-dev-afk <vaibhavdhiman265@gmail.com>
2026-02-24 18:01:42 +0530

    -

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express= require('express');
 const cors = require('cors');
-const {GoogleGenerativeAI} = require('@google/generatie-ai');
+const {GoogleGenerativeAI} = require('@google/generative-ai');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
